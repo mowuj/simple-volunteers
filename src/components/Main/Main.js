9e72e71e795160ccfd2400a4f0a52841c0dc87dc
mowuj <mowuj92@gmail.com>
2022-04-06 14:58:53 +0600

    simple search

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,21 +1,27 @@
 import React, { useEffect, useState } from 'react';
+import useVolunteers from '../../hooks/useVolunteers';
 import Activity from '../Activity/Activity';
 import './Main.css'
 
 const Main = () => {
-    const [volunteers, setVolunteers] = useState([]);
+    const [volunteers, setVolunteers] = useVolunteers();
 
-    useEffect(() => {
-        fetch('data.json')
-        .then(res=>res.json())
-        .then(data=>setVolunteers(data))
-    },[])
+    const [searchResult,setSearchResult]=useState([])
+
+    const searchChange = event => {
+        const searchText = event.target.value;
+        const match = volunteers.filter(v=>v.title.toLowerCase().includes(searchText))
+        setSearchResult(match)
+    }
     return (
         <div>
             <h2>Volunteers Activities:{volunteers.length }</h2>
+            <div className='search-input'>
+                <input onChange={searchChange} type="text" placeholder='search' />
+            </div>
             <div className='activity-container'>
                 {
-                volunteers.map(activity => <Activity
+                searchResult.map(activity => <Activity
                     key={activity._id}
                     activity={activity}
                 ></Activity>)
--- src/components/Volunteers/Volunteers.js
+++ src/components/Volunteers/Volunteers.js
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import useVolunteers from '../../hooks/useVolunteers';
+import Activity from '../Activity/Activity';
+import './Volunteers.css'
 const Volunteers = () => {
+    const [volunteers,setVolunteers]=useVolunteers()
     return (
         <div>
             <h2>All Volunteers</h2>
+            <div className="activity-conainter">
+                {
+                    volunteers.map(activity => <Activity
+                        key={activity._id}
+                        activity={activity}
+                    ></Activity>)
+                }
+            </div>
         </div>
     );
 };
